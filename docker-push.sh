88116b4033e55b262f6b8d5ff5b9e6d7903aeeca
yarikgoldvarg <40838787+yarikgoldvarg@users.noreply.github.com>
2020-08-05 10:27:18 +0300

    Update docker-push.sh

--- docker-push.sh
+++ docker-push.sh
@@ -5,7 +5,7 @@ COMMIT=$(git rev-parse --short HEAD)
 TAG="latest"
 
 docker login
-sudo docker build  . -t $PROJECT:$COMMIT
+docker build  . -t $PROJECT:$COMMIT
 docker tag $PROJECT:$COMMIT $REPO/$PROJECT:$TAG
 #docker push $REPO/$PROJECT:$TAG
 
